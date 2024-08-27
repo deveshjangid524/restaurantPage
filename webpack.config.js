7e2a8bb502dde63900da9e1b1035b2b7965f0322
Devesh Jangid <deveshjangid524@gmail.com>
2024-08-27 13:33:07 +0530

    create webpack , handle the configurations , install packages

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|avif)$/i,
+        type: 'asset/resource',  // This will handle image files
+      },
+    ],
+  },
+};
